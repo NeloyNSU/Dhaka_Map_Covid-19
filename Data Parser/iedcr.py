a91d684b8d9b4eb9068054600629f900ea0335b5
NeloyNSU <asif.neloy@northsouth.edu>
2020-06-08 01:00:42 +0600

    initialized

---
+++ Data Parser/iedcr.py
@@ -0,0 +1,12 @@
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import re
+
+webPage = urllib.request.urlopen('https://iedcr.gov.bd/').read()
+soupObject = BeautifulSoup(webPage,'html.parser')
+tags = soupObject('td')
+
+i = 0
+while i<12:
+    print(tags[i].contents[0],tags[i+3].contents[0])
+    i+=4
